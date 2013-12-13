0ace898dc1286be59421e622a6c9774b95892e88
Frantisek Havluj <moskyt@rozhled.cz>
2013-12-13 08:52:51 +0100

    09,10

---
+++ 2013_2014/09/scrape/vendor/bundle/ruby/1.9.1/gems/nokogiri-1.6.0/ext/nokogiri/tmp/x86_64-apple-darwin13.0.0/ports/libxslt/1.1.26/libxslt-1.1.26/xsltConf.sh
@@ -0,0 +1,7 @@
+#
+# Configuration file for using the xslt library
+#
+XSLT_LIBDIR="-L/Users/haf/research/imf/2013_2014/09/scrape/vendor/bundle/ruby/1.9.1/gems/nokogiri-1.6.0/ports/x86_64-apple-darwin13.0.0/libxslt/1.1.26/lib"
+XSLT_LIBS="-lxslt  -L/Users/haf/research/imf/2013_2014/09/scrape/vendor/bundle/ruby/1.9.1/gems/nokogiri-1.6.0/ports/x86_64-apple-darwin13.0.0/libxml2/2.8.0/lib -lxml2 -lz -lpthread -liconv -lm "
+XSLT_INCLUDEDIR="-I/Users/haf/research/imf/2013_2014/09/scrape/vendor/bundle/ruby/1.9.1/gems/nokogiri-1.6.0/ports/x86_64-apple-darwin13.0.0/libxslt/1.1.26/include"
+MODULE_VERSION="xslt-1.1.26"
