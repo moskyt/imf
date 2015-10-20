b62ad31213df1fe82efffc13f4f1ae7e39315ef0
Frantisek Havluj <moskyt@rozhled.cz>
2015-10-20 12:14:19 +0200

    imf 1516 04

---
+++ 2015_2016/03/package/ruby/chyba3.rb
@@ -0,0 +1,15 @@
+# v souboru 'data.txt' najde nejmensi a nejvetsi cislo
+
+max = -1e99
+min = +1e99
+IO.readlines("data.txt").each do |line|
+  ary = line.strip.split.map(&:to_f)
+  ary.each do |x|
+    if x > max
+      max = x
+    end
+    min = x if x < min
+  end
+end
+puts "Minimum = #{min}"
+puts "Maximum = #{max}"
